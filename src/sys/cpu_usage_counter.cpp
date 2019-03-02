e7cffbae3d7664f7cfe6071c1f444894f205a88a
freecss <karlfreecss@gmail.com>
2019-03-02 11:58:52 +0800

    add cpu usage count

--- src/sys/cpu_usage_counter.cpp
+++ src/sys/cpu_usage_counter.cpp
@@ -61,13 +61,68 @@ int is_all_num(const string & str){
     return true;
 }
 
-void dir_order(const char pathname[]){
+string get_uid(const string & file_name){
+    string uid = "";
+    int fd = open(file_name.c_str(), O_RDONLY, S_IWRITE | S_IREAD);
+    if (fd < 0){
+        ERROR_EXIT(__FILE__, __LINE__, "read_file_error");
+        return uid;
+    }
+    char buff[MAX_BUFFER_SIZE];
+    memset(buff, 0, sizeof(buff));
+    int len = read(fd, buff, MAX_BUFFER_SIZE);
+    close(fd);
+
+    vector<string> res = split(buff, '\n');
+    for (int i = 0; i < res.size(); ++i) {
+        if (strncmp(res[i].c_str(), "Uid", 3) == 0){
+            string uid_str = res[i].substr(4);
+            int begin = -1, end = -1;
+            for (int i = 0; i < uid_str.length(); ++i){
+                if (uid_str[i] == '\t' & begin != -1){
+                    end = i;
+                    break;
+                }
+                if (uid_str[i] != '\t' & begin == -1){
+                    begin = i;
+                }
+            }
+            if (end > 0){
+                uid = uid_str.substr(begin, end - begin);
+            }
+        }
+    }
+    return uid;
+}
+
+unsigned int get_cpu_count(const string & file_name){
+    unsigned int cpu_count = 0;
+    int fd = open(file_name.c_str(), O_RDONLY, S_IWRITE | S_IREAD);
+    if (fd < 0){
+        ERROR_EXIT(__FILE__, __LINE__, "read_file_error");
+        return cpu_count;
+    }
+    char buff[MAX_BUFFER_SIZE];
+    memset(buff, 0, sizeof(buff));
+    int len = read(fd, buff, MAX_BUFFER_SIZE);
+    close(fd);
+
+    vector<string> res = split(buff, ' ');
+    unsigned long utime = atoi(res[13].c_str());
+    unsigned long stime = atoi(res[14].c_str());
+    printf("%lu %lu\n", utime, stime);
+    cpu_count = utime + stime;
+    return cpu_count;
+}
+
+map<string, unsigned long> walk_proc(const char pathname[]){
     DIR *dfd;
     char name[MAX_PATH];
     struct dirent *dp;
+    map<string, unsigned long> uid_count;
     if ((dfd = opendir(pathname)) == NULL){
         printf("dir_order: can't open %s\n %s", pathname,strerror(errno));
-        return;
+        return uid_count;
     }
     while ((dp = readdir(dfd)) != NULL){
         if (strncmp(dp->d_name, ".", 1) == 0)
@@ -78,39 +133,20 @@ void dir_order(const char pathname[]){
             if (is_all_num(dp->d_name)){
                 memset(name, 0, sizeof(name));
                 sprintf(name, "%s/%s/status", pathname, dp->d_name);
-                //FILE * fp = fopen(name, "wb");
-                int fd = open(name, O_RDONLY, S_IWRITE | S_IREAD);
-                if (fd < 0){
-                    ERROR_EXIT(__FILE__, __LINE__, "read_file_error");
-                    return ;
+                string uid = get_uid(name);
+
+                sprintf(name, "%s/%s/stat", pathname, dp->d_name);
+                unsigned int cpu_usage = get_cpu_count(name);
+                if (uid_count.find(uid) == uid_count.end()){
+                    uid_count[uid] = 0;
                 }
-                char buff[MAX_BUFFER_SIZE];
-                memset(buff, 0, sizeof(buff));
-                int len = read(fd, buff, MAX_BUFFER_SIZE);
-                close(fd);
-                vector<string> res = split(buff, '\n');
-                for (int i = 0; i < res.size(); ++i) {
-                    if (strncmp(res[i].c_str(), "Uid", 3) == 0){
-                        string uid_str = res[i].substr(4);
-                        int begin = -1, end = -1;
-                        for (int i = 0; i < uid_str.length(); ++i){
-                            if (uid_str[i] == '\t' & begin != -1){
-                                end = i;
-                                break;
-                            }
-                            if (uid_str[i] != '\t' & begin == -1){
-                                begin = i;
-                            }
-                        }
-                        if (end - begin> 3){
-                            printf("%s : %s\n", dp->d_name, res[i].c_str());
-                        }
-                    }
-                }
+                uid_count[uid] += cpu_usage;
+                cout << name << ' '<< uid << ' ' << cpu_usage << endl;
             }
         }
     }
     closedir(dfd);
+    return uid_count;
 }
 
 map<string, string> get_uid_name(){
@@ -133,7 +169,10 @@ int main(){
     for (auto x : dict){
         cout << x.first << ' ' << x.second << endl;
     }
-    dir_order("/proc");   
+    map<string, unsigned long> res = walk_proc("/proc");   
+    for (auto x : res){
+        cout << x.first << ' ' << x.second << endl;
+    }
     return 0;
 }
 
